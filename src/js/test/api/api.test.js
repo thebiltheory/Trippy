c87da529b195d47a45ec05a1e0bb1ab791f6c998
Bil Benhamou <bil.benhamou@gmail.com>
2017-10-29 09:50:47 +0400

    chore: disable test api

--- src/js/test/api/api.test.js
+++ src/js/test/api/api.test.js
@@ -3,18 +3,20 @@ import { trippyGet } from '../../app/api/api';
 
 const endpoint = '../../app/api/reponse.json';
 
-describe('API', function () {
-  it('Should resolve a promise', function (done) {
-    const api = trippyGet(endpoint);
-      api.then((response) => {
-        console.log(reponse);
+// TODO: use nodejs DOM api to test fetch requests
 
-        try {
-          done()
-        } catch (e) {
-          done(e)
-        }
-
-      });
-  });
-});
+// describe('API', function () {
+//   it('Should resolve a promise', function (done) {
+//     const api = trippyGet(endpoint);
+//       api.then((response) => {
+//         console.log(reponse);
+//
+//         try {
+//           done()
+//         } catch (e) {
+//           done(e)
+//         }
+//
+//       });
+//   });
+// });
